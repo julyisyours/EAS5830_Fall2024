18788c40b87bf4c12a28dfcc7a0777510b1353c4
Songnan Jiang <31149486+julyisyours@users.noreply.github.com>
2024-10-20 17:34:14 -0400

    Update connect_to_eth.py
    changes v2

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -3,22 +3,13 @@ from web3 import Web3
 from web3.middleware import geth_poa_middleware
 from web3.providers.rpc import HTTPProvider
 
-'''If you use one of the suggested infrastructure providers, the url will be of the form
-now_url  = f"https://eth.nownodes.io/{now_token}"
-alchemy_url = f"https://eth-mainnet.alchemyapi.io/v2/{alchemy_token}"
-infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
-'''
-
-
 def connect_to_eth():
-	url = f"https://mainnet.infura.io/v3/8ef000923e5f43da92da6cfbe71ccd34"  # FILL THIS IN
-	w3 = Web3(HTTPProvider(url))
-	assert w3.is_connected(), f"Failed to connect to provider at {url}"
-	return w3
-
+    url = f"https://mainnet.infura.io/v3/8ef000923e5f43da92da6cfbe71ccd34"  # Your Infura URL here
+    w3 = Web3(HTTPProvider(url))
+    assert w3.is_connected(), f"Failed to connect to provider at {url}"
+    return w3
 
 def connect_with_middleware(contract_json):
-  
     # Read contract address and ABI from the JSON file
     with open(contract_json, "r") as f:
         d = json.load(f)
@@ -41,10 +32,8 @@ def connect_with_middleware(contract_json):
 
     return w3, contract
 
-
-
 if __name__ == "__main__":
-	connect_to_eth()
-	w3, contract = connect_with_middleware("contract_info.json")
-  print(f"Connected to Web3: {w3.is_connected()}")
-  print(f"Contract Address: {contract.address}")
+    w3, contract = connect_with_middleware("contract_info.json")
+    print(f"Connected to Web3: {w3.is_connected()}")
+    print(f"Contract Address: {contract.address}")
+
