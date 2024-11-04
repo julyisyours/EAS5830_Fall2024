09ee55ebf2bcedce17f38d1ea197c54ab26450d9
Songnan Jiang <31149486+julyisyours@users.noreply.github.com>
2024-11-04 15:25:50 -0500

    Add files via upload

--- verify.py
+++ verify.py
@@ -16,20 +16,22 @@ with open("NFT.abi", "r") as abi_file:
 
 # Set up the contract
 contract = w3.eth.contract(address=contract_address, abi=abi)
+my_address = "0xAe693a9b03B2Ef5F642f82d910e19D7b47Bb87B7"
+my_private_key = "0xfb91cee50e8cbcbb250f17a85ca66b0f8b915cf27764aeccce611915ae022caf"
 
 # Generate a unique nonce for the claim function
 nonce = w3.eth.get_transaction_count(0xAe693a9b03B2Ef5F642f82d910e19D7b47Bb87B7)
 
 # Claim the NFT
 transaction = contract.functions.claim(nonce).buildTransaction({
-    'from': 0xAe693a9b03B2Ef5F642f82d910e19D7b47Bb87B7,
+    'from': my_address,
     'gas': 2000,
     'gasPrice': w3.toWei('30', 'gwei'),
     'nonce': nonce,
 })
 
 # Sign and send the transaction
-signed_txn = w3.eth.account.sign_transaction(transaction, private_key=0xfb91cee50e8cbcbb250f17a85ca66b0f8b915cf27764aeccce611915ae022caf)
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=my_private_key)
 txn_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 
 # Wait for transaction confirmation
@@ -41,7 +43,7 @@ def signChallenge( challenge ):
     w3 = Web3()
 
     #This is the only line you need to modify
-    sk = "0xfb91cee50e8cbcbb250f17a85ca66b0f8b915cf27764aeccce611915ae022caf"
+    sk = my_private_key 
 
     acct = w3.eth.account.from_key(sk)
 
